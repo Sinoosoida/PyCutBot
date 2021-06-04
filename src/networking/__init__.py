82c91398493814b50af28cde10c4aa4bef5fd29c
EgorVoron <ekvoron01@gmail.com>
2021-06-04 16:37:39 +0300

    :recycle: Refactoring

--- src/networking/__init__.py
+++ src/networking/__init__.py
@@ -1,34 +1,29 @@
-from pytube import Playlist
 from pytube import YouTube
-import sqlite3
-import json
-import requests
 import os
 from google.oauth2 import service_account
-from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
+from googleapiclient.http import MediaFileUpload
 from googleapiclient.discovery import build
+import warnings
 
 
-def find_best_resolution_stream(link):
-    yt = YouTube(link)
+def find_best_resolution_stream(yt_obj):
     res = 0
-    for i in yt.streams:
-        if not (i.resolution is None):
-            if int(i.resolution[0:-1]) > res:
-                res = int(i.resolution[0:-1])
-                stream = i
-    return stream
+    best_resolution_stream = yt_obj.streams[0]
+    for stream in yt_obj.streams[1:]:
+        if stream.resolution and int(stream.resolution[0:-1]) > res:
+            res = int(stream.resolution[0:-1])
+            best_resolution_stream = stream
+    return best_resolution_stream
 
 
-def find_best_abr_stream(link):
-    yt = YouTube(link)
+def find_best_abr_stream(yt_obj):
     abr = 0
-    for i in yt.streams:
-        if not (i.abr is None) and (i.type == "audio"):
-            if int(i.abr[0:-4]) > abr:
-                abr = int(i.abr[0:-4])
-                stream = i
-    return stream
+    best_abr_stream = yt_obj.streams[0]
+    for stream in yt_obj.streams[1:]:
+        if stream.abr and stream.type == "audio" and int(stream.abr[0:-4]) > abr:
+            abr = int(stream.abr[0:-4])
+            best_abr_stream = stream
+    return best_abr_stream
 
 
 def send_to_google_drive(file_path, name):
@@ -43,23 +38,19 @@ def send_to_google_drive(file_path, name):
         'parents': [folder_id]
     }
     media = MediaFileUpload(file_path, resumable=True)
-    r = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    service.files().create(body=file_metadata, media_body=media, fields='id').execute()
 
 
-def download_from_youtube(link):
-    all_streams = YouTube(link).streams
-    if len(all_streams.filter(only_audio=True)) == 0:
-        video_path = all_streams.get_highest_resolution().download()
-        return os.path.basename(video_path), None
-    else:
-        video_path = find_best_resolution_stream(link).download()
-        stream = find_best_abr_stream(link)
-        audio_path = stream.download()
-        return os.path.basename(video_path), os.path.basename(audio_path)
-
-
-def get_name(link):
-    return YouTube(link).title
+#
+#
+# def download_from_youtube(link):
+#     all_streams = YouTube(link).streams
+#     if len(all_streams.filter(only_audio=True)) == 0:
+#         video_path = all_streams.get_highest_resolution().download()
+#         return os.path.basename(video_path), None
+#     video_path = find_best_resolution_stream(link).download()
+#     audio_path = find_best_abr_stream(link).download()
+#     return os.path.basename(video_path), os.path.basename(audio_path)
 
 
 def good_link(link):
@@ -68,25 +59,38 @@ def good_link(link):
     return True
 
 
-def download_video(link, path="./"):
-    yt = YouTube(link)
-    res = 0
-    for i in yt.streams:
-        if not (i.resolution is None):
-            if int(i.resolution[0:-1]) > res:
-                res = int(i.resolution[0:-1])
-                stream = i
-    video_path = stream.download(output_path=path)
-    return video_path
+def get_yt_object(link):
+    return YouTube(link)
 
 
-def download_audio(link, path="./"):
-    yt = YouTube(link)
-    abr = 0
-    for i in yt.streams:
-        if not (i.abr is None) and (i.type == "audio"):
-            if int(i.abr[0:-4]) > abr:
-                abr = int(i.abr[0:-4])
-                stream = i
-    audio_path = stream.download(output_path=path)
-    return audio_path
+# def get_info(yt_obj: YouTube):
+#     return {'title': yt_obj.title,
+#             'description': yt_obj.description,
+#             ''
+#             'thumbnail_url': yt_obj.thumbnail_url
+#             }
+
+
+def download_video(yt_obj, path="./"):
+    return find_best_resolution_stream(yt_obj).download(output_path=path)
+
+
+def download_audio(yt_obj, path="./"):
+    return find_best_abr_stream(yt_obj).download(output_path=path)
+
+
+def download(link, video_dir, audio_dir):
+    yt_object = YouTube(link)
+    video_name = yt_object.title
+    print(video_name)
+    video_path = download_video(link, video_dir)
+    print(video_path)
+    audio_path = download_audio(link, audio_dir)
+    print(video_path)
+    if not audio_path:
+        warnings.warn(message="have not audio", category=UserWarning, stacklevel=1)
+    if not video_path:
+        warnings.warn(message="have not audio", category=UserWarning, stacklevel=1)
+    print(str(video_path) + " " + str(audio_path))
+    print("downloading done")
+    return video_name, video_path, audio_path, yt_object
