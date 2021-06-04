8a097dd6a414ca6ffc51df6abf7d9343aa41d9f5
EgorVoron <ekvoron01@gmail.com>
2021-06-04 21:02:05 +0300

    :art: Add thumbnails handling

--- src/networking/__init__.py
+++ src/networking/__init__.py
@@ -3,6 +3,22 @@ from google.oauth2 import service_account
 from googleapiclient.http import MediaFileUpload
 from googleapiclient.discovery import build
 import warnings
+import requests
+import os
+
+
+def download_thumbnail(yt_object: YouTube, image_dir):
+    url = yt_object.thumbnail_url
+    full_path = os.path.join(image_dir, 'thumbnail.png')
+    with open(full_path, 'wb') as handle:
+        response = requests.get(url, stream=True)
+        if not response.ok:
+            return None
+        for block in response.iter_content(1024):
+            if not block:
+                break
+            handle.write(block)
+    return full_path
 
 
 def find_best_resolution_stream(yt_obj):
@@ -58,17 +74,22 @@ def get_yt_object(link):
     return YouTube(link)
 
 
-def download(yt_object, video_dir, audio_dir):
+def download(yt_object: YouTube, video_dir, audio_dir, thumbnail_dir):
     video_name = yt_object.title
     print(video_name)
     video_path = download_video(yt_object, video_dir)
     print(video_path)
     audio_path = download_audio(yt_object, audio_dir)
     print(video_path)
+    thumbnail_path = download_thumbnail(yt_object, thumbnail_dir)
+    print(thumbnail_path)
     if not audio_path:
         warnings.warn(message="have not audio", category=UserWarning, stacklevel=1)
     if not video_path:
         warnings.warn(message="have not audio", category=UserWarning, stacklevel=1)
+    if not thumbnail_path:
+        warnings.warn(message="have not thumbnail", category=UserWarning, stacklevel=1)
     print(str(video_path) + " " + str(audio_path))
     print("downloading done")
-    return video_name, video_path, audio_path
+
+    return video_name, video_path, audio_path, thumbnail_path
--- uploading.py
+++ uploading.py
@@ -1,5 +1,5 @@
 import datetime
-from Google import Create_Service
+from google_api_utils import Create_Service
 from googleapiclient.http import MediaFileUpload
 
 CLIENT_SECRET_FILE = r"client_secret.json"
@@ -9,32 +9,34 @@ SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-upload_date_time = datetime.datetime(2020, 12, 25, 12, 30, 0).isoformat() + '.000Z'
 
-request_body = {
-    'snippet': {
-        'categoryI': 19,
-        'title': 'Upload Testing',
-        'description': 'Hello World Description',
-        'tags': ['Travel', 'video test', 'Travel Tips']
-    },
-    'status': {
-        'privacyStatus': 'private',
-        'publishAt': upload_date_time,
-        'selfDeclaredMadeForKids': False,
-    },
-    'notifySubscribers': False
-}
+def upload_video_to_youtube(video_path, thumbnail_path, title, description, tags):
+    upload_date_time = datetime.datetime(2020, 12, 25, 12, 30, 0).isoformat() + '.000Z'
 
-mediaFile = MediaFileUpload('test_video.mp4')
+    request_body = {
+        'snippet': {
+            'categoryI': 27,  # education
+            'title': title,
+            'description': description,
+            'tags': tags
+        },
+        'status': {
+            'privacyStatus': 'private',
+            'publishAt': upload_date_time,
+            'selfDeclaredMadeForKids': False,
+        },
+        'notifySubscribers': False
+    }
 
-response_upload = service.videos().insert(
-    part='snippet,status',
-    body=request_body,
-    media_body=mediaFile
-).execute()
+    mediaFile = MediaFileUpload(video_path)
 
-service.thumbnails().set(
-    videoId=response_upload.get('id'),
-    media_body=MediaFileUpload('test_tumbnail.png')
-).execute()
+    response_upload = service.videos().insert(
+        part='snippet,status',
+        body=request_body,
+        media_body=mediaFile
+    ).execute()
+
+    service.thumbnails().set(
+        videoId=response_upload.get('id'),
+        media_body=MediaFileUpload(thumbnail_path)
+    ).execute()
