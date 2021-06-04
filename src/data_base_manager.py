4eeaa37504bc836b31f96d5f003e74b6fd48d2f1
Sinoosoida <tolkomouyashik@mail.ru>
2021-06-04 20:28:37 +0300

    db manager update

--- src/data_base_manager.py
+++ src/data_base_manager.py
@@ -1,5 +1,14 @@
 from networking import *
 from db import SQLParser
+from pytube import *
+
+
+def get_videos_from_channel(channel):
+    return Channel(channel).video_urls
+
+
+def get_videos_from_playlist(playlist):
+    return Playlist(playlist).video_urls
 
 
 def videos_from_channel(data_base):
@@ -12,3 +21,8 @@ def videos_from_playlists(data_base):
     for playlist in data_base.get_all("playlist"):
         for video in get_videos_from_playlist(playlist[0]):
             data_base.save("video", video, "in queue")
+
+
+def update_videos(data_base):
+    videos_from_playlists(data_base)
+    videos_from_channel(data_base)
