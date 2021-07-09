243204e616be74ad8f49f54a9dca47259875ce51
Sinoosoida <tolkomouyashik@mail.ru>
2021-07-09 20:50:54 +0300

    manager seems to be done. Catching bugs

--- src/db/data_base_manager/videos_updater.py
+++ src/db/data_base_manager/videos_updater.py
@@ -3,6 +3,9 @@ import datetime
 from src.requests_utils import get_request_with_retries
 import json
 import src.config as config
+from src.db.mongo_parser import Status, MongoParser, Collection
+from datetime import datetime
+from src.config import mongo_password, mongo_username
 
 
 def get_videos_urls_since_date(channel_url, date=datetime.datetime.min):
@@ -49,26 +52,38 @@ def get_all_playlists(channel_url: str, key=config.api_key):
     return playlists_list
 
 
-def get_videos_from_channel(channel):
+def get_videos_url_from_channel(channel):
     return Channel(channel).video_urls
 
 
-def get_videos_from_playlist(playlist):
+def get_videos_url_from_playlist(playlist):
     return Playlist(playlist).video_urls
 
 
-def videos_from_channel(data_base):
-    for channel in data_base.get_all("channel"):
-        for video in get_videos_from_channel(channel[0]):
-            data_base.save("video", video, "in queue")
+def videos_from_channel(parser):#done
+    for channel in parser.get_all("channel"):
+        last_request_time = channel.last_request_datetime
+        parser.set(collection_name = "channel", url = channel.url, last_request_datetime=datetime.now())
+        for video_url in get_videos_urls_since_date(channel.url, last_request_time):
+            parser.save(collection_name="video", url=video_url, status="in queue")
 
 
-def videos_from_playlists(data_base):
-    for playlist in data_base.get_all("playlist"):
-        for video in get_videos_from_playlist(playlist[0]):
-            data_base.save("video", video, "in queue")
+def videos_from_playlists(parser):#done
+    for playlist in parser.get_all("playlist"):
+        for video_url in get_videos_url_from_playlist(playlist.url):
+            parser.save(collection_name="video", url=video_url, status="in queue")
+            parser.add_playlist_to_video(video_url, playlist.url)
+
+def playlists_from_channel(parser):
+    for channel in parser.get_all("channel"):
+        for video_url in get_all_playlists(channel.url)
+            parser.save(collection_name="playlist", url=video_url)
 
 
-def update_videos(data_base):
-    videos_from_playlists(data_base)
-    videos_from_channel(data_base)
+def update_videos(data_base):#    playlists_from_channel(parser)
+    parser = MongoParser(atlas=True,
+                         username=mongo_username,
+                         password=mongo_password)
+    playlists_from_channel(parser)
+    videos_from_channel(parser)
+    videos_from_playlists(parser)#обязатльно последний
