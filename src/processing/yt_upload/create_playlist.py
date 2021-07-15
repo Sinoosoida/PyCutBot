b2215e1eb33947954c8224bb359cd1b452cbde58
EgorVoron <ekvoron01@gmail.com>
2021-07-15 21:39:25 +0300

    :boom: add creating playlists!

---
+++ src/processing/yt_upload/create_playlist.py
@@ -0,0 +1,33 @@
+from pytube import Playlist
+from src.processing.yt_upload.google_api_utils import Create_Service
+import os
+
+
+def get_abs_path(relative_path):
+    dir_name = os.path.dirname(__file__)
+    return os.path.join(dir_name, relative_path)
+
+
+def create_playlist(old_playlist_url, app_version=5):
+    playlist = Playlist(old_playlist_url)
+
+    CLIENT_SECRET_FILE = get_abs_path(f'..\..\google_api\client_secrets\client_secret_{app_version}.json')
+    API_NAME = "youtube"
+    API_VERSION = "v3"
+    SCOPES = ['https://www.googleapis.com/auth/youtube.upload',
+              'https://www.googleapis.com/auth/youtube']
+
+    service = Create_Service(
+        CLIENT_SECRET_FILE, API_NAME, API_VERSION, app_version, SCOPES
+    )
+    request_body = {
+        "snippet": {
+            "title": playlist.title,
+            "description": f"Плейлист с канала {playlist.owner}, оригинал: {old_playlist_url}",
+        }
+    }
+
+    service.playlists().insert(
+        part="snippet",
+        body=request_body,
+    ).execute()
