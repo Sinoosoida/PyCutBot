b396b3e4b3e1035980338d1c58cb967887166b5d
EgorVoron <ekvoron01@gmail.com>
2021-08-04 20:40:09 +0300

    :bug: cringe f-string fix

--- src/processing/yt_upload/upload.py
+++ src/processing/yt_upload/upload.py
@@ -13,7 +13,7 @@ def get_abs_path(relative_path):
 @with_retries(3)
 def upload_video_to_youtube(video_path, title, description, tags,
                             thumbnail_path=None, app_version=5) -> str:
-    CLIENT_SECRET_FILE = get_abs_path('..\..\google_api\client_secrets\client_secret_{app_version}.json')
+    CLIENT_SECRET_FILE = get_abs_path(f'..\..\google_api\client_secrets\client_secret_{app_version}.json')
     API_NAME = 'youtube'
     API_VERSION = 'v3'
     SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
