91b45c32c31bf1563c4aa2ec948d8931952d2147
EgorVoron <ekvoron01@gmail.com>
2021-08-18 11:58:25 +0300

    :alembic: fix load_audio

--- src/processing/core/load_audio.py
+++ src/processing/core/load_audio.py
@@ -4,17 +4,21 @@ from pympler import asizeof
 import audio2numpy
 
 
-def convert_audio_to_mp3(path):
-    song = AudioSegment.from_file(path, "mp3")
-    song.export(path, format="mp3",
+def convert_audio_to_mp3(path, new_path):
+    song = AudioSegment.from_file(path, "webm")
+    song.export(new_path, format="mp3",
                 bitrate="320k")
 
 
 def load_audio(path):
-    convert_audio_to_mp3(path)
-    return audio2numpy.open_audio(path)
+    new_path = '.'.join(path.split('.')[::-1]) + '.mp3'
+    convert_audio_to_mp3(path, new_path)
+    return audio2numpy.open_audio(new_path)
+
 
 # convert_audio_to_mp3(orig_path)
 
 # y, sr = librosa.load(orig_path, sr=320)
 # librosa.
+
+load_audio(r'C:\Users\79161\PycharmProjects\PyCutBot\media\input_audio\test video.webm')
