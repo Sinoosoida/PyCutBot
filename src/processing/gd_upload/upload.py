7373f75d1323d6640554e464c593b9bfa1ad96d9
Sinoosoida <tolkomouyashik@mail.ru>
2021-09-05 21:28:27 +0300

    up to drive done

--- src/processing/gd_upload/upload.py
+++ src/processing/gd_upload/upload.py
@@ -6,67 +6,54 @@ import pprint
 import os
 
 import google.oauth2
-#import service_account
+# import service_account
 from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
 from googleapiclient.discovery import build
 import pprint
 import io
 import shutil
 
-zip_file_dir="../"
-zip_file_name="packed_fles"
-dir_of_unpacked_files="../"
-name_of_unziped_files="media"
-type_of_archive="zip"
-
-def pack_files ():
-    if os.path.exists(zip_file_dir+zip_file_name+"."+type_of_archive):
-        os.remove(zip_file_dir+zip_file_name+"."+type_of_archive)
-    shutil.make_archive(zip_file_dir+zip_file_name,
-                        type_of_archive,
-                        dir_of_unpacked_files,
-                        name_of_unziped_files)
-    shutil.rmtree(dir_of_unpacked_files, ignore_errors=False, onerror=None)
-
-def unpack_files (zip_file_path ,dir_of_unpacked_files ):
-    shutil.unpack_archive(zip_file_dir+zip_file_name+"."+type_of_archive, dir_of_unpacked_files)
-
-def rm_local_zip_file():
-    pass
-
-def rm_local_media_files():
-    shutil.rmtree()
-
-def upload_path(path, folder_id="1PGHW4Crd2PYZdhIZT8a69pG4Di7Q6gn7", new_name=None):
-    # filename, file_extension = os.path.splitext(path)
-    # if file_extension==None:#если это папка
-    #     new_folder_id = create_folder_on_google_drive(new_name, folder_id)
-    #     files = os.listdir(path)
-    #     for file in files:
-    #         upload_path(path+filse, new_folder_id, None)
-    #     return new_folder_id
-    # else:
-    #     upload_file_on_google_drive()
-    # print(filename)
-    # print(file_extension)
-    # files = os.listdir(path)
-    pass
+zip_file_dir = "../"
+zip_file_name = "packed_fles"
+dir_of_unpacked_files = "../"
+name_of_unziped_files = "media"
+type_of_archive = "zip"
+keys_path = "google_drive_api.json"
 
 
-def upload_to_prod_google_drive(title, main_folder_id="1PGHW4Crd2PYZdhIZT8a69pG4Di7Q6gn7"):
-    # SCOPES = ['https://www.googleapis.com/auth/drive']
-    # SERVICE_ACCOUNT_FILE = './google_drive_api.json'
-    # credentials = service_account.Credentials.from_service_account_file(
-    #     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-    # service = build('drive', 'v3', credentials=credentials)
-    # folder_id = main_folder_id
-    # file_metadata = {
-    #     'name': name,
-    #     'parents': [folder_id]
-    # }
-    # media = MediaFileUpload(file_path, resumable=True)
-    # r = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-    pass
+def pack_files():
+    if os.path.exists(dir_of_unpacked_files + name_of_unziped_files):
+        if os.path.exists(zip_file_dir + zip_file_name + "." + type_of_archive):
+            os.remove(zip_file_dir + zip_file_name + "." + type_of_archive)
+        shutil.make_archive(zip_file_dir + zip_file_name,
+                            type_of_archive,
+                            dir_of_unpacked_files,
+                            name_of_unziped_files)
+        # shutil.rmtree(dir_of_unpacked_files+name_of_unziped_files, ignore_errors=False, onerror=None)
+
+
+def unpack_files():
+    if os.path.exists(zip_file_dir + zip_file_name + "." + type_of_archive):
+        if os.path.exists(dir_of_unpacked_files + name_of_unziped_files):
+            shutil.rmtree(dir_of_unpacked_files + name_of_unziped_files, ignore_errors=False, onerror=None)
+        shutil.unpack_archive(zip_file_dir + zip_file_name + "." + type_of_archive, dir_of_unpacked_files,
+                              type_of_archive)
+        # os.remove(zip_file_dir + zip_file_name + "." + type_of_archive)
+
+
+def upload_to_pub_google_drive(video_path, title, main_folder_id="1PGHW4Crd2PYZdhIZT8a69pG4Di7Q6gn7"):
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+    credentials = service_account.Credentials.from_service_account_file(
+        keys_path, scopes=SCOPES)
+    service = build('drive', 'v3', credentials=credentials)
+    file_metadata = {
+        'name': title,
+        'parents': [main_folder_id]
+    }
+    media = MediaFileUpload(video_path, resumable=True)
+    r = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    print(r)
+    return (r)
 
 
 def download_from_prod_google_drive(file_id):
@@ -81,20 +68,21 @@ def download_from_prod_google_drive(file_id):
     pass
 
 
-def upload_to_pub_google_drive(title, main_floader_id="1PGHW4Crd2PYZdhIZT8a69pG4Di7Q6gn7"):
-    # SCOPES = ['https://www.googleapis.com/auth/drive']
-    # SERVICE_ACCOUNT_FILE = './google_drive_api.json'
-    # credentials = service_account.Credentials.from_service_account_file(
-    #     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-    # service = build('drive', 'v3', credentials=credentials)
-    # folder_id = main_floader_id
-    # file_metadata = {
-    #     'name': name,
-    #     'parents': [folder_id]
-    # }
-    # media = MediaFileUpload(file_path, resumable=True)
-    # r = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-    pass
+def upload_to_prod_google_drive(title, main_folder_id="1PGHW4Crd2PYZdhIZT8a69pG4Di7Q6gn7"):
+    pack_files()
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+    credentials = service_account.Credentials.from_service_account_file(
+        keys_path, scopes=SCOPES)
+    service = build('drive', 'v3', credentials=credentials)
+    file_metadata = {
+        'name': title,
+        'parents': [main_folder_id]
+    }
+    media = MediaFileUpload(zip_file_dir + zip_file_name + "." + type_of_archive, resumable=True)
+    r = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    print(r)
+    return (r)
 
 
-pack_files()
+# pack_files()
+upload_to_prod_google_drive("tets")
