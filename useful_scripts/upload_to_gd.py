b50984f84725c458bdb4871dd14db41b895eae9c
EgorVoron <ekvoron01@gmail.com>
2021-11-01 16:09:52 +0300

    upload_to_gd.py

---
+++ useful_scripts/upload_to_gd.py
@@ -0,0 +1,5 @@
+from src.processing.google_drive.google_drive import upload_to_prod_google_drive
+
+upload_to_prod_google_drive(
+    video_path="media/input_video/Алгебра_и_геометрия_16_Прямая_сумма_и_сопряженное_пространство.mp4",
+    title="test_upload")
